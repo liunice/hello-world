95e6ef40d5272bb797f54c13f9df0967c95222b8
liunice <liunice@gmail.com>
2022-08-18 16:56:18 +0800

    Update rrtv3.js

--- rrtv3.js
+++ rrtv3.js
@@ -3,6 +3,7 @@ var root = JSON.parse($response.body);
 for (var i = 0; i < root.data.sortedItems.length; i++) {
     var item = root.data.sortedItems[i];
     item.canPlay = true;
+    item.canShowVip = false;
 }
 root.data.authorityInfo.playRestricted = 0;
 root.data.watchInfo.sortedItems = root.data.sortedItems;
