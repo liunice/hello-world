a25fd909c437eff595ffae795653b2538914086d
liunice <liunice@gmail.com>
2022-08-21 00:41:39 +0800

    Update liulishuo.js

--- liulishuo.js
+++ liulishuo.js
@@ -1,18 +1,154 @@
-var root = JSON.parse($response.body);
+var root = {
+    "cc": {
+        "bought": true,
+        "expiredAtSec": "1671612404"
+    },
+    "darwin": {
+        "usedDays": 0,
+        "bought": true,
+        "premiumIcon": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
 
-var expiredAt = 0x70db6800;
-var startedAt = 0x61ec29ff;
-var remainDays = 0x3e7;
-var usedDays = 365;
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "libra": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
 
-root.cc.bought = true;
-root.cc.expiredAtSec = expiredAt;
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "hifi": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
 
-root.darwin.usedDays = usedDays;
-root.darwin.bought = true;
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "hasBasic": true,
+        "remainDays": 999,
+        "emi": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
 
-root.hasPaidContent = true;
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "name": "BASE",
+        "ricoLiveClass": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
 
-console.log("liulishuo.js invoked!");
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "type": "base",
+        "expired": false,
+        "businessEnglish": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
+
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "alix": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
+
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "expiresAt": 1661612404,
+        "darwin": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1661612404,
+            "vouchers": [
+
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "tourismEnglish": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
+
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "liveClass": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
+
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "homework": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
+
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        },
+        "iosDisableSubscribe": false,
+        "firstBoughtInfo": {
+            "durationInSec": 604800,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1661612404,
+            "isFirstBought": true
+        },
+        "rico": {
+            "amount": 999,
+            "effectedAtSec": 1661007604,
+            "expiredAtSec": 1671612404,
+            "vouchers": [
+
+            ],
+            "bought": true,
+            "subscriptionStatus": 1
+        }
+    },
+    "bell": {
+        "bellPtFinished": true,
+        "expiredAtSec": "1671612404",
+        "liveClassEnabled": true,
+        "type": 1,
+        "bought": true,
+        "startedAtSec": "1661007604"
+    },
+    "hasPaidContent": true
+};
 
 $done({ body: JSON.stringify(root)});
