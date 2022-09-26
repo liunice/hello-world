e6cc786d9ea098c02fd607b1f90b4a277e797e1c
liunice <liunice@gmail.com>
2022-09-27 03:11:27 +0800

    edit

--- hbomax_sub.js
+++ hbomax_sub.js
@@ -54,9 +54,9 @@
     else if (/manifests\.api\.hbo\.com\/hls\.m3u8\?f\.audioTrack=/.test($request.url)) {
         const body = $response.body
         const resolution = body.indexOf('RESOLUTION=3840x2160') > -1 ? '3840x2160' : '1920x1080'
-        const bitrates = [...body.matchAll(RegExp(String.raw`#EXT-X-STREAM-INF:BANDWIDTH=(\d+),AVERAGE-BANDWIDTH=\d+,CODECS="[^"]+",RESOLUTION=${resolution},AUDIO="ac3".*?\s+https:\/\/.+`, 'g'))].map(s => parseInt(s[1]))
+        const bitrates = [...body.matchAll(RegExp(String.raw`#EXT-X-STREAM-INF:BANDWIDTH=(\d+),AVERAGE-BANDWIDTH=\d+,CODECS="(?:avc|hvc)[^"]+",RESOLUTION=${resolution},AUDIO="ac3".*?\s+https:\/\/.+`, 'g'))].map(s => parseInt(s[1]))
         const maxrate = Math.max(...bitrates)
-        const m = RegExp(String.raw`#EXT-X-STREAM-INF:BANDWIDTH=(${maxrate}),AVERAGE-BANDWIDTH=\d+,CODECS="([^"]+)",RESOLUTION=${resolution},AUDIO="ac3".*?\s+(https:\/\/.+)`, 'g').exec(body)
+        const m = RegExp(String.raw`#EXT-X-STREAM-INF:BANDWIDTH=(${maxrate}),AVERAGE-BANDWIDTH=\d+,CODECS="((?:avc|hvc)[^"]+)",RESOLUTION=${resolution},AUDIO="ac3".*?\s+(https:\/\/.+)`, 'g').exec(body)
         if (m) {
             $.log(`found ${resolution}:`, m[3])
             $.setdata(m[3], 'hbomax_hd_hls_url')
